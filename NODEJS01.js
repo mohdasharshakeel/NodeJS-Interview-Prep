27994b6d223cea08e1afa48514117e704c22ca03
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-08-20 11:15:44 +0530

    nodeJS interview

---
+++ NODEJS01.js
@@ -0,0 +1,16 @@
+// let var const 
+// spread operator vs rest operator 
+// arrow function vs normal function
+// promices vs callback vs async await [ call back hell]
+// what is closure 
+// event babbling vs evennt capturing 
+// what is event loop in nodeJS 
+// difference between process.xt setImigate vs setTimeout 
+// async vs sync 
+// require vs import
+// middleware in NodeJS
+// what is expressJS
+// how i can handle error in nodeJS
+// how can we connect NodeJS in mongoDB database 
+// authentication vs authorization 
+
