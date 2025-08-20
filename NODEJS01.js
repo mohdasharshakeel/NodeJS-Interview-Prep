4695d9ef848f6b6ec744f46929aafeda95da38a9
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-08-20 11:50:15 +0530

    complete 1st interview questions

--- NODEJS01.js
+++ NODEJS01.js
@@ -13,4 +13,26 @@
 // how i can handle error in nodeJS
 // how can we connect NodeJS in mongoDB database 
 // authentication vs authorization 
-
+// suppose i am building e-commerce application [ aur mere pas user ke kafi alag alag collections hai like user's cart , user's order,   to hum sabko ek jagah kaise dikha sakte hai | how i can join  multiple collections ]
+// what is populate method 
+// how can we perform serch pagination filter sort in one single API 
+// nodeJS microservice architeture 
+// MVC structure 
+// how to work with social login like google login, facebook login 
+// web aur phone dono me ye kam krne ke liye hum kaise bna skte hai 
+// payment getway kaise work krta hai 
+// agar user payment kr rha hai aur uska internet bnd ho jata hai to mere server ko kaise pata chlega ke user ka internet bnd hua hai aur usne payment kiya hai  || we can use the assite properties 
+// chatapplication 
+// how can we securie nodeJS application 
+// notification feature [ job scheduling ]
+// work with any email features [ node mailer , transporter]
+// how can we implement multiple email 
+// what is multer 
+// If i want to upload 3 or 4GB movie so how can we do with third party library 
+// how can i encrypte our password 
+// JWT password 
+// forgote password funcationlty [ how many apis we have to create ]
+// what is cors method 
+// how we can create nodeJS server without express method 
+// pagination logic 
+// how we can optimize our search quairy 
